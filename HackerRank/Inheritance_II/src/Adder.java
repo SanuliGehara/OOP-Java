f2a8e20846d395b6386dbff9e54fd0a5607105e7
SanuliGehara <geharasanuli5@gmail.com>
2024-08-22 23:34:17 +0530

    Complete Inheritance-II question

---
+++ HackerRank/Inheritance_II/src/Adder.java
@@ -0,0 +1,5 @@
+public class Adder extends Arithmetic{
+    void details() {
+        System.out.println("This is inherited form Arithmetic class");
+    }
+}
---
+++ HackerRank/Inheritance_II/src/Arithmetic.java
@@ -0,0 +1,5 @@
+public class Arithmetic {
+    int add (int x, int y){
+        return x + y;
+    }
+}
