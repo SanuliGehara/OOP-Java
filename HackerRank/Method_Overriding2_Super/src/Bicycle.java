58bca5ad6c15dda1e66e6ad1e1cdfbfe01ae440b
SanuliGehara <geharasanuli5@gmail.com>
2024-08-24 17:00:12 +0530

    Complete method overriding with super question

---
+++ HackerRank/Method_Overriding2_Super/src/Bicycle.java
@@ -0,0 +1,5 @@
+public class Bicycle {
+    String define_me(){
+        return "a vehicle with pedals.";
+    }
+}
---
+++ HackerRank/Method_Overriding2_Super/src/MotorCycle.java
@@ -0,0 +1,13 @@
+public class MotorCycle extends Bicycle{
+    String define_me() {
+        return "a cycle with an engine.";
+    }
+
+    MotorCycle(){
+        System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+        String temp= super.define_me();
+
+        System.out.println("My ancestor is a cycle who is "+ temp );
+    }
+}
