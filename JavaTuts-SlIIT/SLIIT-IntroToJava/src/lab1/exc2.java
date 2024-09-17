a624cfb282396ecd29868ce95b182f8b713d2545
SanuliGehara <geharasanuli5@gmail.com>
2024-09-18 00:04:30 +0530

    complete OOP lab1 exc 2

---
+++ JavaTuts-SlIIT/SLIIT-IntroToJava/src/lab1/exc2.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class exc2 {
+    public static void main(String[] args) {
+        System.out.println("=".repeat(35));
+        System.out.println("=\t\tStudent Information\t\t\t=");
+        System.out.println("=".repeat(35));
+        System.out.println("=Name\t\t\t:ARDP Ranasinghe\t=");
+        System.out.println("=Reg No\t\t\t:DIS\\08\\M4\\1234\t\t=");
+        System.out.println("=Address\t\t:Malabe\t\t\t\t=");
+        System.out.println("=".repeat(35));
+    }
+}
