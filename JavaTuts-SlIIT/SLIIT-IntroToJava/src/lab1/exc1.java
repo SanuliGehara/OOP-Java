01ff38ef134d0126a255f22524e355367418c478
SanuliGehara <geharasanuli5@gmail.com>
2024-09-17 23:54:45 +0530

    complete OOP lab1 exc 1

---
+++ JavaTuts-SlIIT/SLIIT-IntroToJava/src/lab1/exc1.java
@@ -0,0 +1,7 @@
+package lab1;
+
+public class exc1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nIt's been nice knowing you.\nGoodbye world!");
+    }
+}
