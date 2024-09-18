88931b6690213b16635364f22f5e28cacabd99dc
SanuliGehara <geharasanuli5@gmail.com>
2024-09-18 12:44:55 +0530

    complete OOP lab2 exc 2

---
+++ JavaTuts-SlIIT/SLIIT-IntroToJava/src/lab2/Exc2.java
@@ -0,0 +1,9 @@
+package lab2;
+
+public class Exc2 {
+    public static void main(String[] args) {
+        System.out.println("DitNo : " + args[1]);
+        System.out.println("Name : " + args[0]);
+        System.out.println("District : " + args[2]);
+    }
+}
