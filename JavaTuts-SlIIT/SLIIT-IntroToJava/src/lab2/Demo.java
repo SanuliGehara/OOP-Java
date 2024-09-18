2aed0c2ce2062759a0eeee0d2be1eb61313b4d24
SanuliGehara <geharasanuli5@gmail.com>
2024-09-18 19:44:20 +0530

    complete lab2 exc 5

---
+++ JavaTuts-SlIIT/SLIIT-IntroToJava/src/lab2/Demo.java
@@ -0,0 +1,9 @@
+package lab2;
+
+public class Demo {
+    public static void main(String[] args) {
+        EvenOddNumber num = new EvenOddNumber();
+        boolean ans = num.findEvenOrOdd(4);
+        System.out.println(ans);
+    }
+}
---
+++ JavaTuts-SlIIT/SLIIT-IntroToJava/src/lab2/EvenOddNumber.java
@@ -0,0 +1,11 @@
+package lab2;
+
+public class EvenOddNumber {
+    public boolean findEvenOrOdd(int i) {
+        if (i%2 == 0) {
+            //even
+            return true;
+        }
+        return false;
+    }
+}
